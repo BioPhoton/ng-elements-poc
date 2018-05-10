3c67fe113d338a65d13874fd089028e29429053f
Michael Hladky <michael@hladky.at>
2018-05-10 13:48:23 +0200

    feat(create standalone web-component): Implemented remove AppComponent from project

--- projects/my-first-element/src/app/app.module.ts
+++ projects/my-first-element/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { FirstElementComponent } from './first-element/first-element.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstElementComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [FirstElementComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }

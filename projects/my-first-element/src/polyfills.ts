edc8ff19e6a2ba26a8e7caf12785470178760f07
Michael Hladky <michael@hladky.at>
2018-05-10 15:39:25 +0200

    deleted old files

--- projects/my-first-element/src/polyfills.ts
+++ projects/my-first-element/src/polyfills.ts
@@ -78,3 +78,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import '@webcomponents/custom-elements/custom-elements.min.js';
+import '@webcomponents/custom-elements/src/native-shim.js';

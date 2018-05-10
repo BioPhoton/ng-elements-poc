42d0d19f8bc01e45c841a184a612db0588b029f0
Michael Hladky <michael@hladky.at>
2018-05-10 14:06:37 +0200

    feat(create standalone web-component): setup component and bootstrapping

--- projects/my-first-element/src/app/app.module.ts
+++ projects/my-first-element/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import { FirstElementComponent } from './first-element/first-element.component';
+import {createCustomElement, NgElementConfig} from '@angular/elements';
 
 @NgModule({
   declarations: [FirstElementComponent],
   imports: [BrowserModule],
-  providers: [],
-  bootstrap: []
+  entryComponents: [FirstElementComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+
+  }
+
+  ngDoBootstrap() {
+    const config: NgElementConfig = {injector: this.injector};
+    const ngElement = createCustomElement(FirstElementComponent, config);
+
+    customElements.define('app-first-element', ngElement);
+  }
+
+}
